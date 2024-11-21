de6ad9b858afdb654901396a279abc14d7ed474e
Damian <dg997678@gmail.com>
2024-11-21 14:54:28 -0500

    Add validations

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,10 +1,15 @@
-import { IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
   @IsString()
+  @IsEmail()
   email: string;
 
   @IsString()
   @MinLength(8)
   password: string;
+
+  @IsString()
+  @MinLength(1)
+  name: string;
 }
